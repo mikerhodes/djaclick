784647adbffa0ea1f1ded09b8c6a618204bf60e6
Mike Rhodes <mike.rhodes@gmail.com>
2024-01-05 10:47:16 +0000

    Add charts that group by method, status and path

--- djaclick/hits/static/hits/main.js
+++ djaclick/hits/static/hits/main.js
@@ -1,71 +1,79 @@
-console.log("Hello world");
+// main.js renders the charts for hits into canvases
+// defined on the page. The canvas elements should have
+// class=chart and, optionally, data-group-by=status/path/method.
 
-let myChart = undefined;
+// Store charts by their canvas ID for later updates
+const charts = new Map();
 
-async function redrawChart() {
-  const res = await fetch("/hits/timeseries");
+// Default configuration for all charts
+const defaultConfig = {
+  type: "line",
+  options: {
+    pointStyle: false,
+    borderWidth: 1,
+    animation: { duration: 0 },
+    scales: {
+      x: { type: "time" },
+      y: { min: 0, stacked: true },
+    },
+  },
+};
+
+async function redrawChart(canvasElement) {
+  const groupBy = canvasElement.dataset.groupBy;
+  let res = undefined;
+  if (!groupBy) {
+    res = await fetch("/hits/timeseries");
+  } else {
+    res = await fetch(`/hits/timeseries?groupby=${groupBy}`);
+  }
   const json = await res.json();
-  const rawdata = json["data"];
-  console.log(rawdata);
 
-  let minDate = "a"; // "a" is after any valid date.
-  rawdata.forEach((x) => {
-    if (x.ts < minDate) {
-      minDate = x.ts;
-    }
+  // Create chart.js friendly datasets by mapping
+  // to x,y and find the min X and maxY as we do so.
+  const datasets = [];
+  let minDate = Number.MAX_SAFE_INTEGER;
+  let maxY = Number.MIN_SAFE_INTEGER;
+  json["series"].forEach((rawdata) => {
+    const data = [];
+
+    rawdata["data"].forEach((row) => {
+      const ts = Date.parse(row.ts);
+      minDate = Math.min(row.ts, minDate);
+      maxY = Math.max(row.count, maxY);
+
+      data.push({ x: ts, y: row.count });
+    });
+
+    datasets.push({
+      label: rawdata["label"],
+      data: data,
+      fill: "stack",
+    });
   });
 
-  const data = {
-    // labels: rawdata.map((row) => row.ts),
-    datasets: [
-      {
-        label: "Hits count",
-        data: rawdata.map((row) => {
-          // return { x: `${row.year}-11-06 23:39:30`, y: row.count };
-          return { x: `${row.ts}`, y: row.count };
-        }),
-      },
-    ],
-  };
-  if (!myChart) {
-    const config = {
-      type: "line",
-      data: data,
-      options: {
-        animation: {
-          duration: 0,
-        },
-        scales: {
-          x: {
-            type: "time",
-            min: minDate,
-            time: {
-              // unit: "minute",
-            },
-          },
-          y: {
-            min: 0,
-            suggestedMax: 800,
-          },
-        },
-      },
-    };
-
-    myChart = new Chart(
-      document.getElementById("acquisitions"),
-      config,
-    );
-  } else {
-    myChart.options.scales.x.min = minDate;
-    myChart.data = data;
-    myChart.update();
+  const chartId = canvasElement.id;
+  if (!charts.has(chartId)) {
+    charts[chartId] = new Chart(canvasElement, structuredClone(defaultConfig));
   }
+  const chart = charts[chartId];
+  chart.options.scales.x.min = minDate;
+  chart.options.scales.y.suggestedMax = maxY;
+  chart.data = { datasets: datasets };
+  chart.update();
+}
+
+function redrawAllCharts() {
+  const matches = document.querySelectorAll("canvas.chart");
+  matches.forEach((n) => {
+    redrawChart(n);
+  });
 }
 
 (async function () {
   const button = document.querySelector("#getmorehits");
   button.addEventListener("click", (event) => {
-    redrawChart();
+    redrawAllCharts();
   });
-  redrawChart();
+  redrawAllCharts();
 })();
--- djaclick/hits/views.py
+++ djaclick/hits/views.py
@@ -3,28 +3,49 @@ from django.http import JsonResponse
 
 import clickhouse_connect
 
+LABEL_COL = 2
+TS_COL = 0
+COUNT_COL = 1
+
 
 def index(request):
-    context = {
-        "latest_question_list": "foo",
-    }
+    context = {}
     return render(request, "hits/index.html", context)
 
+
 def timeseries(request):
-    client = clickhouse_connect.get_client(
-        host="localhost", port=8123)
+    # Check for groupby in the query string and check it is valid
+    group_by = request.GET.get("groupby")
+    if group_by is not None:
+        if group_by not in ["'all'", "method", "status", "path"]:
+            return JsonResponse({"ok": False, "reason": "bad groupby"}, status=400)
+    else:
+        # use a static string for grouping, to allow query to return the
+        # same result shape for simpler processing
+        group_by = "'all'"
 
-    QUERY = """SELECT
-        toStartOfInterval(ts, toIntervalSecond(5)) AS h,
-        count(status)
-    FROM hits
-    GROUP BY h
-    ORDER BY h DESC
-    LIMIT 20
-    """
-
+    # Group hits into 5s boundaries. Only get upto now-10s to avoid the
+    # last point being incompletely counted
+    client = clickhouse_connect.get_client(host="localhost", port=8123)
+    QUERY = """
+        SELECT
+            toStartOfInterval(ts, toIntervalSecond(5)) AS h,
+            count(*), {0}
+        FROM hits
+        WHERE h > subtractMinutes(now(), 5) AND h < subtractSeconds(now(), 10)
+        GROUP BY h, {0}
+        ORDER BY h DESC
+    """.format(  # Not great, but we check for valid values above, so safe.
+        group_by
+    )
     result = client.query(QUERY)
 
-    return JsonResponse({"data": [{"ts": x[0], "count": x[1]} for x in result.result_rows]})
-    
-   
+    # The rows are interleaved for the groupby due to the order by;
+    # create a series for each value of groupby before returning
+    series = {}
+    for row in result.result_rows:
+        points = series.setdefault(row[LABEL_COL], [])
+        points.append({"ts": row[TS_COL], "count": row[COUNT_COL]})
+    return JsonResponse(
+        {"ok": True, "series": [{"label": k, "data": v} for k, v in series.items()]}
+    )
